56ca171d1f4d403238ff145aad79d9ff15c3646d
Lyle <denmanlp@gmail.com>
2015-11-21 16:51:39 -0600

    One of the goat doors opens after a door is selected

--- src/js/main.js
+++ src/js/main.js
@@ -1,18 +1,40 @@
-function randomWinDoor() {
+var goatDoors = [];
 
+function pickWinner() {
     return Math.floor((Math.random() * 3) + 1);
 }
-var winDoor = randomWinDoor();
-console.log(winDoor);
+var winDoor = pickWinner();
+console.log('Winner:',winDoor);
 
-var showGoat = function (e) {
-    e.src = "img/goat.png";
-    console.log(-1 != e.className.indexOf("door1"));
-
-    for (var i = 0; i < document.getElementById('threeDoors').getElementsByTagName('*'); i++)
-    {
-        console.log(i);
+for (var i = 1; i <= 3; i++) {
+    if (i != winDoor) {
+        goatDoors.push(i);
     }
-};
+}
 
-window.onLoad = randomWinDoor();
+var firstDoorOpened = false;
+function showGoat(e) {
+
+    if (!firstDoorOpened) {
+
+        for (door in goatDoors) {
+            // If the selected door is found in the goatDoors list:
+            if (e.className.indexOf(goatDoors[door]) != -1) {
+                // Remove the selected door from the delete list
+                goatDoors.splice(door, 1);
+            }
+        }
+
+        // Open one of the doors or the only door in the loseDoor list
+        var showDoor = Math.floor(Math.random() * goatDoors.length);
+        var elem = document.getElementById('threeDoors');
+        var child = elem.children;
+        openFirstDoor(child[goatDoors[showDoor]-1]);
+    }
+
+}
+
+function openFirstDoor(door) {
+    door.src = "img/goat.png";
+    firstDoorOpened = true;
+}
