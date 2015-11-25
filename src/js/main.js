a8e43fc0506df701369e59dbaea321814d2d9db0
Lyle Denman <denmanlp@gmail.com>
2015-11-24 19:37:12 -0600

    Move initial state into main.js
    Wanted the app to have its own initial state and reset to that state instead of using location reload.

--- src/js/main.js
+++ src/js/main.js
@@ -3,26 +3,47 @@
 
 (function() {
 
-    var winDoor = Math.floor((Math.random() * 3) + 1);
-    var goatDoors = []; // Holds losing doors
+    var doorsOpened;
+    var goatDoors; // Holds losing doors
     var child = document.getElementById('threeDoors').children;
+    var door1 = child[0];
+    var door2 = child[1];
+    var door3 = child[2];
     var gameText = document.getElementById('gameText');
+    var button = document.getElementById('resetBtn');
+    var winDoor;
 
-    for (var i = 1; i <= 3; i++) {
-        if (i != winDoor) {
-            goatDoors.push(i);
+    // Initial state
+    function resetState() {
+        doorsOpened = 0;
+        goatDoors = [];
+        door1.src = "img/door1.png";
+        door1.className = "door door1";
+        door2.src = "img/door2.png";
+        door2.className = "door door2";
+        door3.src = "img/door3.png";
+        door3.className = "door door3";
+        button.className = "hide";
+        gameText.innerHTML = "Hello and welcome to Three Doors. Only one of these doors is the Winning Door. The rules are simple: Please select a door.";
+
+        winDoor = Math.floor((Math.random() * 3) + 1);
+        for (var i = 1; i <= 3; i++) {
+            if (i != winDoor) {
+                goatDoors.push(i);
+            }
         }
     }
 
-    var doorsOpened = 0;
     function openDoor(doorEl) {
 
         if (doorsOpened == 0) {
             for (var door in goatDoors) {
                 // If the selected door is found in the goatDoors list:
-                if (doorEl.className.indexOf(goatDoors[door]) != -1) {
-                    // Remove the selected door from the delete list
-                    goatDoors.splice(door, 1);
+                if (goatDoors.hasOwnProperty(door)) {
+                    if (doorEl.className.indexOf(goatDoors[door]) != -1) {
+                        // Remove the selected door from the delete list
+                        goatDoors.splice(door, 1);
+                    }
                 }
             }
 
@@ -32,8 +53,8 @@
             // Remove class names to avoid calling openDoor() on same door twice
             doorOne.className = "";
             doorOne.src = "img/goat.png";
-            gameText.innerHTML = "I've opened one of the losing doors to reveal: a well-formed goat. \<br> You can now " +
-                "select either the same door again or you may choose the other door. \<br> The choice is up to you.";
+            gameText.innerHTML = "I've opened one of the losing doors to reveal: a well-formed goat. <br> You can now " +
+                "select either the same door again or you may choose the other door. <br> The choice is up to you.";
         }
 
         if (doorsOpened == 1) {
@@ -46,7 +67,6 @@
                 doorEl.src = "img/goat.png";
             }
 
-            var button = document.getElementById('resetBtn');
             button.className += " show";
         }
 
@@ -60,7 +80,8 @@
     });
 
     document.getElementById('reset-btn').addEventListener('click', function() {
-        window.location.reload();
+        resetState();
     });
 
+    resetState();
 })();
